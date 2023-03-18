568dee9364ccd07adb99dc0e40dd8393a01bf1ac
LLumisS <vasa16671@gmail.com>
2023-03-18 19:19:06 +0200

    double-linked list sample

---
+++ list.js
@@ -0,0 +1,41 @@
+'use strict';
+
+class Element {
+  constructor(value) {
+    this.next = null;
+    this.prev = null;
+    this.value = value;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  length() {}
+
+  appent() {}
+
+  insert() {}
+
+  delete() {}
+
+  deleteAll() {}
+
+  get() {}
+
+  clone() {}
+
+  reverse () {}
+
+  findFirst() {}
+
+  findLast() {}
+
+  clear() {}
+
+  extend() {}
+}
